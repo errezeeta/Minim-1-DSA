278516b671cc97c3b56768d740914c529a4ca7c2
Angelfm98 <angel.franco@estudiantat.upc.edu>
2021-04-19 01:25:25 +0200

    prueba

---
+++ src/main/java/edu/upc/dsa/mapa/bar/productos.java
@@ -0,0 +1,25 @@
+package edu.upc.dsa.mapa.bar;
+
+public class productos {
+    float precio;
+    int cantidad;
+
+
+
+    public float getPrecio() {
+       return this.precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+}
